86e68108495a2b12ad812c25ef38e19f22b0247b
Arthur Borges <arthurborges.dev@gmail.com>
2021-09-07 11:37:51 -0300

    set up redux configuration files to get the details of country from the API

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import covidReducer from './covid/covid';
+import detailsReducer from './covid/details';
 
 const reducer = combineReducers({
   covidReducer,
+  detailsReducer,
 });
 
 const store = createStore(
