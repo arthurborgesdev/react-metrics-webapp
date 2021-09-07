a9ab8e2cfbc3708160e9328a6b07497b9a25297d
Arthur Borges <arthurborges.dev@gmail.com>
2021-09-07 19:23:09 -0300

    Fix bug of not displaying data when the country name has more than 1 word

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,26 +7,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import Header from './Header';
 import getDetails from '../redux/slices/detailsSlice';
 
-const queryURL = (url, countryData) => {
-  const urlArr = url.split('');
-  urlArr.shift();
-  const urlName = urlArr.join('');
-  const countryName = urlName[0].toUpperCase() + urlName.substring(1);
-  const countryInfo = countryData[countryName];
-  return countryInfo;
-};
-
 const Details = ({ name }) => {
   const { url } = useRouteMatch();
   const dispatch = useDispatch();
 
   const countryData = useSelector((state) => state.detailsReducer.covidDetails);
-  console.log('countryData', countryData);
-  const countryInfo = queryURL(url, countryData);
+  const countryInfo = countryData[name];
 
   useEffect(async () => {
     if (!countryData.length) {
-      console.log('entrei no use effect', name);
       await dispatch(getDetails(url));
     }
   }, []);
