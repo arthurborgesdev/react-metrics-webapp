183c54aed80df11073c7cfc4d4deaaf939be62a4
Arthur Borges <arthurborges.dev@gmail.com>
2021-09-08 12:22:15 -0300

    Finish the styling of alternate colors bars of the details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -21,54 +21,56 @@ const Details = ({ name }) => {
   }, []);
 
   return (
-    <div className="country-details">
+    <div className="country-container">
       <Header title="Contry details" backButtonTitle="< Countries" />
-      <h2>{countryInfo?.name}</h2>
-      <p>
-        Source:
-        {' '}
-        {countryInfo?.source}
-      </p>
-      <p>
-        Deaths until today:
-        {' '}
-        {countryInfo?.today_deaths}
-      </p>
-      <p>
-        Deaths today:
-        {' '}
-        {countryInfo?.today_new_deaths}
-      </p>
-      <p>
-        Confirmed cases until today:
-        {' '}
-        {countryInfo?.today_confirmed}
-      </p>
-      <p>
-        Confirmed cases today:
-        {' '}
-        {countryInfo?.today_new_confirmed}
-      </p>
-      <p>
-        Open cases until today:
-        {' '}
-        {countryInfo?.today_open_cases}
-      </p>
-      <p>
-        Recovered cases until today:
-        {' '}
-        {countryInfo?.today_recovered}
-      </p>
-      <p>
-        New open cases:
-        {' '}
-        {countryInfo?.today_new_open_cases}
-      </p>
-      <p>
-        New recovered cases:
-        {' '}
-        {countryInfo?.today_new_recovered}
-      </p>
+      <div className="country-details">
+        <h2>{countryInfo?.name}</h2>
+        <p>
+          Source:
+          {' '}
+          {countryInfo?.source}
+        </p>
+        <p>
+          Deaths until today:
+          {' '}
+          {countryInfo?.today_deaths}
+        </p>
+        <p>
+          Deaths today:
+          {' '}
+          {countryInfo?.today_new_deaths}
+        </p>
+        <p>
+          Confirmed cases until today:
+          {' '}
+          {countryInfo?.today_confirmed}
+        </p>
+        <p>
+          Confirmed cases today:
+          {' '}
+          {countryInfo?.today_new_confirmed}
+        </p>
+        <p>
+          Open cases until today:
+          {' '}
+          {countryInfo?.today_open_cases}
+        </p>
+        <p>
+          Recovered cases until today:
+          {' '}
+          {countryInfo?.today_recovered}
+        </p>
+        <p>
+          New open cases:
+          {' '}
+          {countryInfo?.today_new_open_cases}
+        </p>
+        <p>
+          New recovered cases:
+          {' '}
+          {countryInfo?.today_new_recovered}
+        </p>
+      </div>
     </div>
   );
 };
