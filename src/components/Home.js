dd6f36481c7ef2679fd12e1488f3098d4ce46cfb
Arthur Borges <arthurborges.dev@gmail.com>
2021-09-08 10:40:32 -0300

    Put Link with className of country-card to the top parent of countryList and it solved the area to be all clickable

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,15 +11,20 @@ const Home = () => {
 
   const countries = useSelector((state) => state.covidReducer.covidCountries);
   const countriesList = countries.map((country) => (
-    <div key={country.name[0]} className="country-card">
-      <Link href="/#" to={`${url}${country.name[0].toLowerCase()}`}>
+    <Link
+      key={country.name[0]}
+      href="/#"
+      to={`${url}${country.name[0].toLowerCase()}`}
+      className="country-card"
+    >
+      <div>
         <div key={country.name[0]} id={country.name[0]} className="country-link">
           <p>{country.name[0]}</p>
           <p>{country.confirmed}</p>
           <p>{country.deaths}</p>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   ));
 
   return (
